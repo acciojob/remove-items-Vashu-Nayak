c75f896e23ab492e38c8a7aacdd743f5df00bab1
Vashu Nayak <97188321+Vashu-Nayak@users.noreply.github.com>
2024-11-16 12:19:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+
+let cselect=document.getElementById("colorSelect")
+let button=document.getElementByTagName("input")
+
+button.addEventListener("click" ()=>{
+	let soption=cselect.value;
+
+	if(soption){
+		let options=cselect.options
+		for(let i=0;i<options.length;i++){
+			if(options[i].value==soption){
+				cselect.remove(i)
+				break
+			}
+		}
+	}
+})
